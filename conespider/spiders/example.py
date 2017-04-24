81ef2b09331fdf33f917ff411dd0c455644e3a83
qianqian121 <qianqian141@hotmail.com>
2017-04-24 14:57:36 -0700

    test

--- conespider/spiders/example.py
+++ conespider/spiders/example.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from conespider.items import ConespiderItem
+from bs4 import BeautifulSoup
 
 class ExampleSpider(scrapy.Spider):
     name = "example"
@@ -8,4 +9,8 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['https://www.bing.com/images/search?q=traffic+cone&qs=n&form=QBILPG&sp=-1&pq=traffic+cone&sc=8-12&cvid=9D31384C3A0D47E4A62311383C32F3DE&first=1&cw=1680&ch=843']
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.title
+        # let's only gather Time U.S. magazine covers
+        # url = response.css("div.refineCol ul li").xpath("a[contains(., 'TIME U.S.')]")
+        # yield scrapy.Request(url.xpath("@href").extract_first(), self.parse_page)
